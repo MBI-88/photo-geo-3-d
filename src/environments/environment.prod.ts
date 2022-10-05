7b3d1c81d395311fd8755220679c794342d99c78
MBI <ingmbi8807@gmail.com>
2022-10-05 09:35:56 -0300

    updated application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAACQY3-YywcWeW_wiTTWNOiaHhv0dGJxY",
+    authDomain: "photo-geo-3d.firebaseapp.com",
+    projectId: "photo-geo-3d",
+    storageBucket: "photo-geo-3d.appspot.com",
+    messagingSenderId: "513804970288",
+    appId: "1:513804970288:web:13bd074f1d83a444a680f5"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,21 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { initializeApp } from "firebase/app";
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAACQY3-YywcWeW_wiTTWNOiaHhv0dGJxY",
+    authDomain: "photo-geo-3d.firebaseapp.com",
+    projectId: "photo-geo-3d",
+    storageBucket: "photo-geo-3d.appspot.com",
+    messagingSenderId: "513804970288",
+    appId: "1:513804970288:web:13bd074f1d83a444a680f5"
+  }
+
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
